acb573c39e30a96db90a769985e081dac694181d
justiksss <122178173+justiksss@users.noreply.github.com>
2023-01-12 01:11:59 +0200

    Update disaster.py

--- card_creators/disaster.py
+++ card_creators/disaster.py
@@ -1,6 +1,12 @@
+import json,random
 from base import BaseCreator
 
 
 class DisasterCreator(BaseCreator):
     
-    data = {}
+    data = json.load(open("../card_data/disaster.json"))
+    def disaster (data):
+
+        disaster = random.choice(list(data.values()))
+        print(disaster)
+    disaster(data)
